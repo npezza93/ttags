36c46428a89be1f9c93c8dae4d3bdbbcdbdc9ef0
Nick Pezza <pezza@hey.com>
2022-12-18 21:15:11 -0500

    Allow passing files through argv

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,13 @@ use std::{fs, str};
 use std::fs::File;
 use std::io::Write;
 use inflector::string::singularize::to_singular;
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let mut context = TagsContext::new();
     let mut tags_file = File::create("tags_output").unwrap();
-
-    // let javascript_config = TagsConfiguration::new(
-    //     tree_sitter_javascript::language(),
-    //     tree_sitter_javascript::TAGGING_QUERY,
-    //     tree_sitter_javascript::LOCALS_QUERY,
-    // ).unwrap();
 
     let ruby_config = TagsConfiguration::new(
         tree_sitter_ruby::language(),
@@ -21,42 +18,43 @@ fn main() {
         tree_sitter_ruby::LOCALS_QUERY,
     ).unwrap();
 
-    let filename = "/Users/nick/Documents/qira/app/models/person.rb";
-    let contents = fs::read(filename).unwrap();
+    for filename in &args[1..] {
+        let contents = fs::read(filename).unwrap();
 
-    let tags = context.generate_tags(&ruby_config, &contents, None).unwrap().0;
+        let tags = context.generate_tags(&ruby_config, &contents, None).unwrap().0;
 
-    for tag in tags {
-        let tag = tag.unwrap();
-        let node_name = ruby_config.syntax_type_name(tag.syntax_type_id);
-        let tag_name = &contents[tag.name_range.start..tag.name_range.end];
-        let original_name = str::from_utf8(<&[u8]>::clone(&tag_name)).unwrap_or("");
+        for tag in tags {
+            let tag = tag.unwrap();
+            let node_name = ruby_config.syntax_type_name(tag.syntax_type_id);
+            let tag_name = &contents[tag.name_range.start..tag.name_range.end];
+            let original_name = str::from_utf8(<&[u8]>::clone(&tag_name)).unwrap_or("");
 
-        let name = name_override(node_name, original_name, tag_name);
+            let name = name_override(node_name, original_name, tag_name, &tag, &contents);
 
-        tags_file.write_all(create_tag(&name, node_name, &tag).as_bytes()).unwrap();
+            tags_file.write_all(create_tag(&name, node_name, &tag, &filename).as_bytes()).unwrap();
 
-        match node_name {
-            "attr_accessor" => {
-                tags_file.write_all(create_tag(&format!("{}=", name), node_name, &tag).as_bytes()).unwrap();
-            },
-            "has_many" => {
-                tags_file.write_all(create_tag(&format!("{}=", name), node_name, &tag).as_bytes()).unwrap();
-                tags_file.write_all(create_tag(&format!("{}_ids", to_singular(&name)), node_name, &tag).as_bytes()).unwrap();
-                tags_file.write_all(create_tag(&format!("{}_ids=", to_singular(&name)), node_name, &tag).as_bytes()).unwrap();
-            },
-            "has_one" | "belongs_to" => {
-                tags_file.write_all(create_tag(&format!("{}=", name), node_name, &tag).as_bytes()).unwrap();
-                tags_file.write_all(create_tag(&format!("build_{}", name), node_name, &tag).as_bytes()).unwrap();
-                tags_file.write_all(create_tag(&format!("create_{}", name), node_name, &tag).as_bytes()).unwrap();
-                tags_file.write_all(create_tag(&format!("create_{}!", name), node_name, &tag).as_bytes()).unwrap();
+            match node_name {
+                "attr_accessor" => {
+                    tags_file.write_all(create_tag(&format!("{}=", name), node_name, &tag, &filename).as_bytes()).unwrap();
+                },
+                "has_many" => {
+                    tags_file.write_all(create_tag(&format!("{}=", name), node_name, &tag, &filename).as_bytes()).unwrap();
+                    tags_file.write_all(create_tag(&format!("{}_ids", to_singular(&name)), node_name, &tag, &filename).as_bytes()).unwrap();
+                    tags_file.write_all(create_tag(&format!("{}_ids=", to_singular(&name)), node_name, &tag, &filename).as_bytes()).unwrap();
+                },
+                "has_one" | "belongs_to" => {
+                    tags_file.write_all(create_tag(&format!("{}=", name), node_name, &tag, &filename).as_bytes()).unwrap();
+                    tags_file.write_all(create_tag(&format!("build_{}", name), node_name, &tag, &filename).as_bytes()).unwrap();
+                    tags_file.write_all(create_tag(&format!("create_{}", name), node_name, &tag, &filename).as_bytes()).unwrap();
+                    tags_file.write_all(create_tag(&format!("create_{}!", name), node_name, &tag, &filename).as_bytes()).unwrap();
+                }
+                _ => {}
             }
-            _ => {}
         }
     }
 }
 
-fn create_tag<'a>(name: &'a str, node_name: &'a str, tag: &'a Tag) -> String {
+fn create_tag<'a>(name: &'a str, node_name: &'a str, tag: &'a Tag, filename: &'a str) -> String {
     let row = tag.span.start.row;
 
     let kind = match node_name {
@@ -69,10 +67,10 @@ fn create_tag<'a>(name: &'a str, node_name: &'a str, tag: &'a Tag) -> String {
     };
 
 
-    format!("{}\t{}\t:{}\t{}\n", name, "app/models/person.rb", row + 1, kind)
+    format!("{}\t{}\t:{}\t{}\n", name, filename, row + 1, kind)
 }
 
-fn name_override<'a>(node_name: &'a str, original_name: &'a str, tag_name: &'a [u8]) -> String {
+fn name_override<'a>(node_name: &'a str, original_name: &'a str, tag_name: &'a [u8], tag: &'a Tag, contents: &'a [u8]) -> String {
     let mut name =
         if original_name.starts_with(':') {
             original_name[1..tag_name.len()].to_string()
