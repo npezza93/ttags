6e69d06c9229ccd60960ea28a3f1ab8ec8e9f35b
Nick Pezza <pezza@hey.com>
2023-03-12 20:40:33 -0400

    Setup initial lsp integration

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 #[macro_use]
 extern crate clap;
+extern crate tower_lsp;
 
 use std::error::Error;
+use tower_lsp::{LspService, Server};
 
 pub mod config;
+pub mod lsp;
 pub mod tag;
 pub mod tagger;
 pub mod ruby;
@@ -12,15 +15,21 @@ pub mod rust;
 
 use config::Config;
 use crate::tagger::Tagger;
+use lsp::Backend;
 
 pub struct App {}
 
 impl App {
-    pub fn run() -> Result<i32, Box<dyn Error>> {
+    #[tokio::main]
+    pub async fn run() -> Result<i32, Box<dyn Error>> {
         let config = Config::new();
 
         if config.lsp {
-            println!("lsp");
+            let stdin = tokio::io::stdin();
+            let stdout = tokio::io::stdout();
+
+            let (service, socket) = LspService::new(|client| Backend { client });
+            Server::new(stdin, stdout, socket).serve(service).await;
         } else {
             Tagger::run(&config, &config.files);
         }
